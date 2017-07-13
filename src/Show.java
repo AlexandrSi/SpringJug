66e588063286b27e97c27efb95545aa358df6bf0
Alexandr <snaiper.bla@gmail.com>
2017-07-13 14:05:53 +0300

    Initial commit

---
+++ src/Show.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Show {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-jong.xml");
+        Performer performer = (Performer) ctx.getBean("duke");
+        performer.perform();
+    }
+}
